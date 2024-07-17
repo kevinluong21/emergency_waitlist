18194007d25d72a747098662f73672902c8d55cc
Kevin Luong <kluon030@uottawa.ca>
2024-07-16 21:27:20 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Triage by JK</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="nav"></div>
+    <div class="container">
+        <h4 class="hospital-name">The Civic Hospital</h4>
+        <h1 class="title">
+            Hospital Wait Times.<br>
+            Made Easy.
+        </h1>
+        <div class="option" id="patient">
+            <h1>I'm a Patient</h1>
+        </div>
+        <div class="option" id="healthcare-provider">
+            <h1>I'm a Healthcare Provider</h1>
+        </div>
+        <div class="option">
+            <h1>I'm an Administrator</h1>
+        </div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
