0d7730e3d61d91be7c5d0d66001c4f8f42c7e858
Jessica Chen <Jessica10chen@gmail.com>
2024-07-22 02:14:16 -0400

    Update patient-display.php

--- patient-display.php
+++ patient-display.php
@@ -2,11 +2,29 @@
 <?php 
 require_once("header.php");
 
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header('Location: patient-login.php');
+    exit();
+}
+
+$query = "SELECT * FROM patients WHERE name = '{$username}'";
+$patient = $pdo->query($query)->fetch();
+
+$wait_time = calculateWaitTime($patient['time_in_queue']);
+
+function calculateWaitTime($time_in_queue) {
+    // todo
+    return 0;
+}
 ?>
-    <div class="container">
-        
     </div>
     <!-- footer -->
+        <h1>Welcome <?php echo htmlspecialchars($username); ?></h1>
+        <p>Your estimated wait time is: <?php echo $wait_time; ?></p>
+        <p>Thank you for your patience.</p>
+    </div>
     <?php 
         require_once("footer.php");
     ?>
